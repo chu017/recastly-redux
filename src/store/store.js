7888f27ec3d6d320346849d83414cecff2897278
calvin <calvinchenhupro@gmail.com>
2021-04-23 10:55:00 -0700

    env setup

--- src/store/store.js
+++ src/store/store.js
@@ -3,4 +3,8 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
+var store = () => {};
+
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+
+export default store;
